d7a2e09683442e2766672e9cfe115159f1719389
Sudal <kimkusyeon@gmail.com>
2022-01-26 17:06:11 +0900

    fix

--- src/components/MyNav.vue
+++ src/components/MyNav.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
 
-    <div class="nav-wrapper">
+    <div class="nav-header">
       <button class="menu"><i class="material-icons">menu</i></button>
       <nav class="label">
         <ul v-for="menu in menus" :key="menu.key">
@@ -11,12 +11,26 @@
       <button class="search"><i class="material-icons">search</i></button>
     </div>
 
-    <div class="dropdown" v-show="isActive">
-      <div class="content">
-        <nav>
-          <ul v-for="sub in dropdownMenus" :key="sub.key">
+    <div class="nav-dropdown" v-if="isActive">
+      <div class="wrapper">
+        <nav class="list">
+          <ul v-for="sub in dropdownMenus" :key="sub.key" class="label">
             <li>{{ sub.label }}</li>
+            <ul v-for="link in sub.link" :key="link" class="link">
+              <li>{{ link }}</li>
+            </ul>
           </ul>
+        </nav>
+        <nav class="shortcut">
+          <ul class="imgs">
+            <li><img src="../assets/health/health02.jpg"></li>
+            <li><img src="../assets/health/health03.jpg"></li>
+          </ul>
+          <div class="btns">
+            <button>인터넷 진료예약</button>
+            <button>예약확인 / 취소</button>
+          </div>
+          <span class="phone">대표전화<em>1588 - 5700</em></span>
         </nav>
       </div>
     </div>
@@ -52,7 +66,7 @@ export default {
   background: var(--white);
   border-bottom: 1px solid #ddd;
 
-  .nav-wrapper {
+  .nav-header {
     display: inherit;
     grid-row: 1 / 2;
     grid-column: 2 / 3;
@@ -78,16 +92,67 @@ export default {
     }
   }
 
-  .dropdown {
+  .nav-dropdown {
     display: inherit;
+    grid-template-columns: 1fr minmax(auto, 110rem) 1fr;
     grid-row: 2 / 3;
-    grid-column: 2 / 3;
-    z-index: 1;
+    grid-column: 1 / 4;
     background: var(--white);
-    border: 1px solid #ddd;
+    border-top: 1px solid #ddd;
+    border-bottom: 2px solid var(--secondary);
+
+    .wrapper {
+      display: inherit;
+      grid-template-columns: 3fr 2fr;
+      grid-column: 2 / 3;
+      border-left: 1px solid #ddd;
+      border-right: 1px solid #ddd;
+
+      .list {
+        display: inherit;
+        grid-template-columns: repeat(3, 1fr);
+        grid-column: 1 / 2;
+        gap: 1.0rem;
+        padding: 2.0rem 4.0rem 2.0rem 4.0rem;
+        line-height: 3.0rem;
+        border-right: 1px solid #ddd;
+      }
+
+      .list > ul > li {
+        margin-bottom: 0.8rem;
+        font-size: 1.6rem;
+        font-weight: bold;
+        letter-spacing: 0.01em;
+      }
+
+      .list > ul > ul > li {
+        font-size: 1.4rem;
+        color: rgb(110, 110, 110);
+      }
+
+      .shortcut {
+        display: inherit;
+        grid-template-rows: repeat(auto-fill, minmax(0, auto));
+        grid-column: 2 / 3;
+        place-items: center center;
+        padding: 4.0rem 0 4.0rem 0;
+
+        .imgs img {
+          width: 30.0rem;
+          height: 8.0rem;
+          margin-bottom: 1.0rem;
+        }
+
+        .btns {
+          display: inherit;
+          grid-template-columns: repeat(auto-fit, auto);
+        }
+        .btns:first-child {
+          justify-items: start;
+        }
+      }
+    }
+
   }
 }
-
-
-
 </style>
